4ac81089ed6446051522110238627f8d0ac08f9b
Javier <javier.oramas@estudiantes.matcom.uh.cu>
2019-12-27 22:48:17 -0500

    compara los archivos de dos directorios y los mantiene iguales

---
+++ script.py
@@ -0,0 +1,33 @@
+import os
+
+f = open("source.txt")
+
+flag = True
+for i in enumerate(f):
+    if flag:
+        source = i
+        flag = False
+    else:
+        destination = i
+source = source[1][0:-1]
+destination = destination[1][:]
+line = "diff -rq "+source+" "+destination+" > result.txt"
+os.system(line)
+
+r = open("result.txt")
+
+for i in enumerate(r):
+    temp = i[1][8:]
+    j = 0
+    while temp[j] != ':':
+        j-= 1
+    
+    file = temp[j+1:]
+    temp = temp[:j]
+    if temp.find(source) != -1:
+        line = "cp "+temp+"/"+file[1:-1]+" "+destination+temp[len(source):]
+        os.system(line)
+    if temp.find(destination) != -1:
+        line = "cp "+temp+"/"+file[1:-1]+" "+source+temp[len(destination):]
+        os.system(line)
+
